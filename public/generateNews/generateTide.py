a53ee70bc13ddeb6c6cd42538899378d279942c3
zq768 <z.nicolezhang@gmail.com>
2025-02-09 00:59:37 -0500

    resolving more merge conflicts

--- public/generateNews/generateTide.py
+++ public/generateNews/generateTide.py
@@ -1,6 +1,5 @@
 import generateUtil 
 import numpy as np
-import pickle
 import random
 
 # will eventually: generate a set of headlines to compare, with a quick summary of viewpoints
@@ -11,17 +10,10 @@ def generate(refresh):
         similarHeadlines = generateUtil.getSimilarHeadlines(left, right, mixed)
         generateUtil.populateBlurbs(similarHeadlines)
         generateUtil.recomputeSimilarities(similarHeadlines, threshold=0.4)
-        
-        with open("similarHeadlines.pkl", "wb") as f:
-            pickle.dump(similarHeadlines, f)
-
-    with open("similarHeadlines.pkl", "rb") as f:
-        similarHeadlines = pickle.load(f)
     
     #choose a headline
-    chosenIndex = random.randint(0, len(similarHeadlines)-1)    
+    chosenIndex = random.randint(0, len(similarHeadlines)-1)   
     generateUtil.writeToCsv(similarHeadlines, chosenIndex)
 
 # run pipeline with a call to generate
-# while True:
 generate(True)
